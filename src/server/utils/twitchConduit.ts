b412f8adf2c8b26b109ffe9443551cf982a8dc6b
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2025-02-03 14:13:50 +0100

    clean up if more than 1 conduit created

--- src/server/utils/twitchConduit.ts
+++ src/server/utils/twitchConduit.ts
@@ -36,8 +36,24 @@ export async function getTwitchConduitId() {
     await DiskCache.set<string>(CACHE_TWITCH_CONDUIT_ID, conduits[0].id);
     return conduits[0].id;
   } else if (conduits.length > 1) {
+    console.error("MORE THAN 1 CONDUIT FOUND, CLEANING UP");
     console.error(conduits);
-    throw new Error("more than 1 conduit created");
+    for (const conduitData of conduits) {
+      console.info(`Deleting conduit id: ${conduitData.id}`);
+      const res = await fetch("https://api.twitch.tv/helix/eventsub/conduits", {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${twitchAppToken}`,
+          "Client-Id": `${process.env.TWITCH_CLIENT_ID}`,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ id: conduitData.id }),
+      });
+      if (!res.ok) {
+        throw new Error("FAILED WHILE DELETING CONDUITS");
+      }
+      console.info(`Deleted conduit id: ${conduitData.id}`);
+    }
   }
   const createConduitResponse = await fetch(TWITCH_CREATE_CONDUIT_URL, {
     method: "POST",
