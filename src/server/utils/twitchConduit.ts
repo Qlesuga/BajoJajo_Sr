ca905b979448360c5cde4d80ef216cb190248c06
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2025-02-03 14:01:03 +0100

    validate conduits

--- src/server/utils/twitchConduit.ts
+++ src/server/utils/twitchConduit.ts
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { DiskCache } from "./cache";
 import { getTwitchAppAccessToken } from "./twitchAuth";
 
@@ -9,12 +10,35 @@ interface TwitchCreateConduitResponse {
   data: [{ id: string; shared_count: number }];
 }
 
+let isConduitValidated = false;
 export async function getTwitchConduitId() {
   const conduitId = await DiskCache.get(CACHE_TWITCH_CONDUIT_ID);
-  if (conduitId) {
+  if (conduitId && isConduitValidated) {
     return conduitId;
   }
   const twitchAppToken = await getTwitchAppAccessToken();
+
+  const checkConduitResponse = await fetch(
+    "https://api.twitch.tv/helix/eventsub/conduits",
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${twitchAppToken}`,
+        "Client-Id": `${process.env.TWITCH_CLIENT_ID}`,
+      },
+    },
+  );
+  const conduits = (
+    (await checkConduitResponse.json()) as TwitchCreateConduitResponse
+  ).data;
+  if (conduits.length == 1) {
+    isConduitValidated = true;
+    await DiskCache.set<string>(CACHE_TWITCH_CONDUIT_ID, conduits[0].id);
+    return conduits[0].id;
+  } else if (conduits.length > 1) {
+    console.error(conduits);
+    throw new Error("more than 1 conduit created");
+  }
   const createConduitResponse = await fetch(TWITCH_CREATE_CONDUIT_URL, {
     method: "POST",
     headers: {
