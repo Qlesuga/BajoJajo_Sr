ca973191e887934d1b8649046069d6bc7e4b1c63
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2025-03-15 10:16:23 +0100

    getAllSongsWithoutBlob

---
+++ src/utils/song/getAllSongsWithoutBlob.ts
@@ -0,0 +1,28 @@
+import { redis } from "lib/redis";
+import type { ISongWithoutBlob } from "types/song";
+import { getSong } from "./getSong";
+
+async function getAllSongsWithoutBlob(
+  broadcasterID: string,
+): Promise<ISongWithoutBlob[]> {
+  const songsID = await redis.lRange(`songs:${broadcasterID}`, 0, -1);
+
+  const songs: (ISongWithoutBlob | null)[] = await Promise.all(
+    songsID.map(async (songID) => {
+      const song = await getSong(songID);
+      if (!song) {
+        return null;
+      }
+      return {
+        title: song.title,
+        songAuthor: song.songAuthor,
+        songLengthSeconds: song.songLengthSeconds,
+        songThumbnail: song.songThumbnail,
+      };
+    }),
+  );
+
+  return songs.filter((song) => song !== null);
+}
+
+export { getAllSongsWithoutBlob };
