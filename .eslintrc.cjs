d1e1a99c53359fc928ae237153812e19270d90d0
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2025-04-04 00:00:19 +0200

    :)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ const config = {
     "prettier",
   ],
   rules: {
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
