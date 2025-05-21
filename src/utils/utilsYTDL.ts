2576c562c9abd02c6554bbef15abb8ae382da357
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2025-05-21 18:23:53 +0200

    fix adding song by url

--- src/utils/utilsYTDL.ts
+++ src/utils/utilsYTDL.ts
@@ -15,7 +15,21 @@ const YT_DLP_API_URL = "http://yt-dlp:8000";
 export async function getYouTubeInfo(
   url: string,
 ): Promise<InfoApiResponse | null> {
-  const info = await fetch(`${YT_DLP_API_URL}/info/${encodeURIComponent(url)}`);
+  const url_split = url.split("?");
+  let final_url;
+  if (url_split.length == 2) {
+    final_url = url_split[1];
+  } else if (url_split.length == 1) {
+    final_url = url_split[0];
+  } else {
+    return null;
+  }
+  if (!final_url) {
+    return null;
+  }
+  const info = await fetch(
+    `${YT_DLP_API_URL}/info/${encodeURIComponent(final_url)}`,
+  );
   if (info.status != 200) {
     return null;
   }
--- yt-dlp/main.py
+++ yt-dlp/main.py
@@ -1,5 +1,6 @@
 import os
 from sys import exception
+from urllib.parse import unquote
 
 import yt_dlp
 from fastapi import FastAPI, Response
@@ -42,11 +43,14 @@ def endpoint_home():
     return {"Hello": ":3"}
 
 
-@app.get("/info/{video_id}", status_code=200)
-def endpoint_video_info(video_id: str, response: Response):
+@app.get("/info/{video}", status_code=200)
+def endpoint_video_info(video: str, response: Response):
     info = {}
+    video_url = video
+    if video[0:2] == "v=":
+        video_url = "https://www.youtube.com/watch?" + video
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = get_video_info(video_id, ydl)
+        info = get_video_info(unquote(video_url), ydl)
 
     if info is False:
         response.status_code = 400
